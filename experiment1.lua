4490caee4a24b69872cf86e5ded11ca5d57b1324
scaryhacker420 <cccom245@gmail.com>
2025-09-01 17:48:15 -0400

    Update experiment1.lua

--- experiment1.lua
+++ experiment1.lua
@@ -1,7 +1,7 @@
 player = 'robnox995'
-if Workspace.Player_Orientation_References then 
+--[[if Workspace.Player_Orientation_References then 
   player = nil
-end
+end]]
 frame = Workspace.NPCS.Eloise.HumanoidRootPart.CFrame
 frame2 = CFrame.new(Vector3.new(0.0,100.0,0.0))
 frame3 = frame2 * frame
