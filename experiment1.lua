312553a5710e13dff4914fe3f4be4a938e88a048
scaryhacker420 <cccom245@gmail.com>
2025-09-01 16:35:21 -0400

    Create experiment1.lua

---
+++ experiment1.lua
@@ -0,0 +1,5 @@
+player = 'robnox995'
+frame = Workspace.NPCS.Eloise.HumanoidRootPart.CFrame
+frame2 = CFrame.new(Vector3.new(0.0,100.0,0.0))
+frame3 = frame2 * frame
+Workspace[player].HumanoidRootPart.CFrame = frame2
