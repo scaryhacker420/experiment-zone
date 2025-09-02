5bc45eab45df4a72eb1fc144f442fdb4d8f6682e
scaryhacker420 <cccom245@gmail.com>
2025-09-01 22:16:47 -0400

    Update experiment1.lua

--- experiment1.lua
+++ experiment1.lua
@@ -1,5 +1,6 @@
 
 frame2 = CFrame.new(Vector3.new(0.0,100.0,0.0))
+frame = CFrame.new(Vector3.new(100.0,100.0,0.0))
 
 local player = Workspace.Player_Orientation_References:GetChildren()[1].Name
 local player_farm = nil
@@ -11,71 +12,88 @@ for _,farm in pairs(Workspace.Farm:GetChildren()) do
 end
 local theplants = player_farm.Important.Plants_Physical
 
+function attributeMatch(obj,pos,neg)
+	local pos = pos or {}
+	local neg = neg or {}
+	for _,attr in ipairs(pos) do 
+		if not obj:GetAttribute(attr) then
+			return false
+		end 
+	end
+	for _,attr in ipairs(neg) do 
+		if obj:GetAttribute(attr) then
+			return false
+		end 
+	end
+	return true
+end
+
+
 function getTrees(output_table,tree_names) 
 	local name_map = {}
 	for i,v in ipairs(tree_names) do 
 		name_map[v]=i
 	end 
-	for _,tree in pairs(theplants:GetChildren()) do
+	for _,tree in ipairs(theplants:GetChildren()) do
 		if name_map[tree.Name] then
 			table.insert(output_table,tree)
 		end
 	end
 end
 
-function getGrownFruitsWithMutation(output_table,tree_list,mutation_list)
-  local mutation_list = mutation_list or {}
-  table.insert(mutation_list,'DoneGrowTime') 
+function getFruitsWithMutations(output_table,tree_list,grown,pos_muts,neg_muts)
+	pos_muts = pos_muts or {}
+	if grown then
+		table.insert(pos_muts,'DoneGrowTime') 
+	end 
 	for _,tree in ipairs(tree_list) do
-		for _,fruit in pairs(tree.Fruits:getChildren()) do 
-			local notmatch = false  
-			for _,mut in ipairs(mutation_list) do 
-				if not fruit:GetAttribute(mut) then
-					notmatch = true
-					break
-				end 
-			end	 
-			if not notmatch then
+		for _,fruit in ipairs(tree.Fruits:getChildren()) do 
+			if attributeMatch(fruit,pos_muts,neg_muts) then
 				table.insert(output_table,fruit)
 			end 
 		end
 	end 
 end 
 
-
-function getGrownFruits(output_table,tree_list)
+function getFruitsWithMutationsLimited(output_table,tree_list,grown,limit,pos_muts,neg_muts)
+	local count = 0
+	pos_muts = pos_muts or {}
+	if grown then
+		table.insert(pos_muts,'DoneGrowTime') 
+	end
 	for _,tree in ipairs(tree_list) do
-		for _,fruit in pairs(tree.Fruits:getChildren()) do
-			if fruit:GetAttribute('DoneGrowTime') then
-				table.insert(output_table)
-			end	
+		for _,fruit in ipairs(tree.Fruits:getChildren()) do 
+			if attributeMatch(fruit,pos_muts,neg_muts) then
+				table.insert(output_table,fruit)
+				count = count + 1
+				if count >= limit then return end
+			end 
 		end
-	end
-end
+	end 
+end 
 
-function findMutatedFruitsInList(output_table,list,mutations)
+function findMutatedFruitsInList(output_table,list,pos_muts,neg_muts)
 	for _,fruit in ipairs(list) do 
-		local notmatch = false
-		for mut in ipairs(mutatation_list) do
-			if not fruit:GetAttribute(mut) then
-				notmatch = true
-				break
-			end
-		end	
-		if not notmatch then
+		if attributeMatch(fruit,pos_muts,neg_muts) then
 			table.insert(output_table,fruit)
-		end
+		end 
 	end
 end
 
-function collectFruits(fruits)
+function collectFruits(fruits,limit)
+	limit = limit or 999999999
+	local count = 0
 	for _,fruit in ipairs(fruits) do 
-		for _,fruit_part in pairs(fruit:GetChildren()) do
-			if fruit_part.IsA('Part') and fruit_part:FindFirstChildOfClass(ProximityPrompt) then
+		for _,fruit_part in ipairs(fruit:GetChildren()) do 
+			if fruit_part:IsA('Part') and fruit_part:FindFirstChildOfClass('ProximityPrompt') then 
 				fireproximityprompt(fruit_part.ProximityPrompt)
 				break
 			end
 		end
+		
+				
+		count = count + 1
+		if count >= limit then return end
 	end
 end
 	
@@ -83,10 +101,10 @@ end
 local trees = {}
 local rfruits = {}
 getTrees(trees,{'Romanesco'})  
-getGrownFruitsWithMutation(rfruits,trees,{'CloudTouched'}) 
+getFruitsWithMutations(rfruits,trees,true) 
 collectFruits(rfruits) 
 
 
 
-Workspace[player].HumanoidRootPart.CFrame = frame2
+Workspace[player].HumanoidRootPart.CFrame = frame
 wait(1)
